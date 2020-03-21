9ef403f50fb03be963466135c287a23db7b72f60
ozacho <worthingtonzach@yahoo.com>
2020-03-20 22:32:15 -0700

    added Flask Projects to be worked on

---
+++ FlaskProjects/Project0/application.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello, World!"
+
+@app.route("/<string:name>")
+def hello(name):
+    name = name.capitalize()
+    return f"<h1>Hello, {name}! </h1>"
