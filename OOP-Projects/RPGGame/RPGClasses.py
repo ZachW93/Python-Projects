fc6c9c05347c98bf8548e1f3f75a915b0ded8ae6
ozacho <worthingtonzach@yahoo.com>
2020-03-24 23:38:13 -0700

    added files from old rpg game to be worked on.

---
+++ OOP-Projects/RPGGame/RPGClasses.py
@@ -0,0 +1,18 @@
+class Pirate:
+
+    def __init__(self, hitpoints, energy):
+        self.hp = hitpoints
+        self.nrg = energy
+        
+        
+class Time_Mage:
+
+    def __init__(self, hitpoints, mana):
+        self.hp = hitpoints
+        self.mana = mana
+        
+class Gunner:
+
+    def __init__(self, hitpoints, energy):
+        self.hp = hitpoints
+        self.nrg = energy
---
+++ OOP-Projects/RPGGame/TimeMageClass.py
@@ -0,0 +1,5 @@
+class Time_Mage:
+
+    def __init__(self, hitpoints, mana):
+        self.hp = hitpoints
+        self.mana = mana
